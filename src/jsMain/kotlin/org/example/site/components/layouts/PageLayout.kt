133ace409700c4708a5ec35b476135dc56615f8d
David Herman <bitspittle@gmail.com>
2022-02-08 11:41:44 -0800

    Create and export basic Kobweb site from template

---
+++ src/jsMain/kotlin/org/example/site/components/layouts/PageLayout.kt
@@ -0,0 +1,28 @@
+package org.example.site.components.layouts
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.css.*
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.silk.components.text.Text
+import kotlinx.browser.document
+import org.jetbrains.compose.web.dom.H1
+import org.example.site.components.sections.NavHeader
+
+@Composable
+fun PageLayout(title: String, content: @Composable () -> Unit) {
+    LaunchedEffect(title) {
+        document.title = title
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize().textAlign(TextAlign.Center),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        NavHeader()
+        H1 { Text(title) }
+        content()
+    }
+}
